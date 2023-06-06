a0dd27946d63ac45382128178e976d377c95cf50
Ethan <theethancastro@gmail.com>
2023-06-06 15:05:17 -0400

    Update health.py

--- health.py
+++ health.py
@@ -7,14 +7,16 @@ st.title('AI Health Coach')
 
 # Get user input
 age = st.number_input('What is your age?', min_value=1, max_value=120)
-weight = st.number_input('What is your weight (in kg)?', min_value=1.0)
-height = st.number_input('What is your height (in cm)?', min_value=1.0)
+weight = st.number_input('What is your weight (in lbs)?', min_value=1.0)
+height = st.number_input('What is your height (height + inches)?', min_value=1.0)
+goal = st.text_input('What is your goal (can be anything health related)?')
+
 
 if st.button('Get advice'):
     # Generate AI response
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=f"I am an AI health coach. You told me that you are {age} years old, weigh {weight} kg and are {height} cm tall. Here's my advice for you: ",
+        prompt=f"I am an AI health coach. You told me that you are {age} years old, weigh {weight} kg, are {height} cm tall, and your goal is {goal}. Here's your BMI, Probable Basal Metabolic Rate, and some advice.: ",
         temperature=0.5,
         max_tokens=100
     )
