742f4b7dfea4a95f853744acb31b076880869fa6
Ethan <theethancastro@gmail.com>
2023-06-06 15:01:02 -0400

    Update health.py

--- health.py
+++ health.py
@@ -1,4 +1,4 @@
-import openai as openai
+import openai
 import streamlit as st
 
 openai.api_key = st.secrets['openai_key']
@@ -13,11 +13,21 @@ height = st.number_input('What is your height (in cm)?', min_value=1.0)
 if st.button('Get advice'):
     # Generate AI response
     response = openai.Completion.create(
-      engine="text-davinci-0",  
-      prompt=f"I am an AI health coach. You told me that you are {age} years old, weigh {weight} kg and are {height} cm tall. Here's my advice for you: ",
-      temperature=0.5,
-      max_tokens=100
+        engine="text-davinci-003",
+        prompt=f"I am an AI health coach. You told me that you are {age} years old, weigh {weight} kg and are {height} cm tall. Here's my advice for you: ",
+        temperature=0.5,
+        max_tokens=100
     )
 
-    # Display AI response
-    st.write(response.choices[0].text.strip())
+    print(response)  # Debug print statement
+
+    if response.choices:
+        print(response.choices[0])  # Debug print statement
+
+        if 'text' in response.choices[0]:
+            # Display AI response
+            st.write(response.choices[0].text.strip())
+        else:
+            st.error("AI response does not contain 'text' attribute.")
+    else:
+        st.error("No choices found in AI response.")
