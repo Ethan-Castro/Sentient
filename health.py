fc6f4b74853c50d9d48d47ed04ddbd3afb65eee6
Ethan <theethancastro@gmail.com>
2023-09-11 20:34:02 -0400

    Update health.py

--- health.py
+++ health.py
@@ -21,15 +21,16 @@ if st.button('Get advice'):
     # Generate AI response
   response = openai.ChatCompletion.create(
         model="gpt-4",
- messages=[
-    {
-    " I am an AI health coach. You told me that you are {age} years old, weigh {weight} lbs, are {height} in tall, and your goal is {goal}. Here's your BMI {bmi} , TDEE or total daily energy expenditure {tdee}, and some advice. repeat the first part then put your own advice: "
-    }
-  ],        temperature=1,
-        max_tokens=250,
+        messages=[
+        {
+         " I am an AI health coach. You told me that you are {age} years old, weigh {weight} lbs, are {height} in tall, and your goal is {goal}. Here's your BMI {bmi} , TDEE or total daily energy expenditure {tdee}, and some advice. repeat the first part then put your own advice: "
+          }
+        ],        
+      temperature=1,
+      max_tokens=250,
       top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
+      frequency_penalty=0, 
+      presence_penalty=0
     )
 
 print(response)  # Debug print statement
