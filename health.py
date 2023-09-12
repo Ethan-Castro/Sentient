84d337253746fa152f1f581c47a9d311bff46898
Ethan <theethancastro@gmail.com>
2023-09-11 20:18:47 -0400

    Update health.py

--- health.py
+++ health.py
@@ -6,14 +6,12 @@ openai.api_key = st.secrets['openai_key']
 st.title('AI Health Coach')
 
 # Get user input
-male = -5
-female = 161
-sex = st.text_input('What is your sex? (male or female)')
+sex = st.number_input('What is your sex? (if male put -5 or female put 161)', min value = -5, max_value = 161)
 age = st.number_input('What is your age?', min_value=1, max_value=100)
 weight = st.number_input('What is your weight (in lbs)?', min_value=1.0)
 height = st.number_input('What is your height (inches)?', min_value=1.0)
 goal = st.text_input('What is your goal (can be anything health related)?')
-activity = st.number_input('Rate your activity level from sedentary to very active (from 1.2 - 1.9)?', min_value=1.2)
+activity = st.number_input('Rate your activity level from sedentary to very active (from 1.2 - 1.9)?', min_value= 1.2, max_value = 1.9)
 bmi = weight / (height * height) * 703
 bmr = 10 * weight + 6.25 * height - 5 * age - sex
 tdee = bmr * sex
