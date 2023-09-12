8f070307448e322a4360f6b4f4d492fce786a9d5
Ethan <theethancastro@gmail.com>
2023-09-11 21:03:44 -0400

    Update health.py

--- health.py
+++ health.py
@@ -21,7 +21,7 @@ if st.button('Get advice'):
     # Generate AI response
 
      prompt_template = (
-    "REPEAT THIS SCRIPT before everytime with the sex, age, weight, height, goal, activity, that the USER puts. I am an AI health coach. You told me that you are {age} years old, weigh {weight} lbs, "
+    "REPEAT THIS SCRIPT before everytime with the sex, age, weight, height, goal, activity, that the USER puts. if it's in brackets put the value they put not the actual word. I am an AI health coach. You told me that you are {age} years old, weigh {weight} lbs, "
     "are {height} inches tall, and your goal is {goal}. Here's your BMI {bmi}, TDEE or total daily energy expenditure {tdee} calories, "
     "and some advice. "
     "{advice}. Repeat this everytime and input the proper values"
