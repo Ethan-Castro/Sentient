42ba82be3d0fc3fb8c9aa1161d2ded486e040e14
Ethan <theethancastro@gmail.com>
2023-09-11 20:23:03 -0400

    Update health.py

--- health.py
+++ health.py
@@ -20,8 +20,8 @@ tdee = bmr * sex
 if st.button('Get advice'):
     # Generate AI response
     response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"I am an AI health coach. You told me that you are {age} years old, weigh {weight} lbs, are {height} in tall, and your goal is {goal}. Here's your BMI {bmi} , TDEE or total daily energy expenditure {tdee}, and some advice.: ",
+        model="gpt-4",
+        prompt=f"I am an AI health coach. You told me that you are {age} years old, weigh {weight} lbs, are {height} in tall, and your goal is {goal}. Here's your BMI {bmi} , TDEE or total daily energy expenditure {tdee}, and some advice. repeat the first part then put your own advice: ",
         temperature=0.5,
         max_tokens=100
     )
