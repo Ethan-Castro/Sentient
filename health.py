9e82b59272840a742cd607e950b365d508dd4533
Ethan <theethancastro@gmail.com>
2023-09-11 20:16:21 -0400

    Update health.py
    Updated

--- health.py
+++ health.py
@@ -6,19 +6,24 @@ openai.api_key = st.secrets['openai_key']
 st.title('AI Health Coach')
 
 # Get user input
-age = st.number_input('What is your age?', min_value=1, max_value=120)
-weight = st.number_input('What is your weight (in kgs)?', min_value=1.0)
-height = st.number_input('What is your height (feet)?', min_value=1.0)
+male = -5
+female = 161
+sex = st.text_input('What is your sex? (male or female)')
+age = st.number_input('What is your age?', min_value=1, max_value=100)
+weight = st.number_input('What is your weight (in lbs)?', min_value=1.0)
+height = st.number_input('What is your height (inches)?', min_value=1.0)
 goal = st.text_input('What is your goal (can be anything health related)?')
-bmr_male = 66 + 13.7 * weight + 5 * height - 6.8 * age
-
+activity = st.number_input('Rate your activity level from sedentary to very active (from 1.2 - 1.9)?', min_value=1.2)
+bmi = weight / (height * height) * 703
+bmr = 10 * weight + 6.25 * height - 5 * age - sex
+tdee = bmr * sex
 
 
 if st.button('Get advice'):
     # Generate AI response
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=f"I am an AI health coach. You told me that you are {age} years old, weigh {weight} kg, are {height} cm tall, and your goal is {goal}. Here's your BMI, Basal Metabolic Rate, and some advice.: ",
+        prompt=f"I am an AI health coach. You told me that you are {age} years old, weigh {weight} lbs, are {height} in tall, and your goal is {goal}. Here's your BMI {bmi} , TDEE or total daily energy expenditure {tdee}, and some advice.: ",
         temperature=0.5,
         max_tokens=100
     )
@@ -27,7 +32,7 @@ if st.button('Get advice'):
 
     if response.choices:
         print(response.choices[0])  # Debug print statement
-        print("Your BMR (Male) is:", bmr_male)
+        print("Your BMR is:", bmr)
 
 if 'text' in response.choices[0]:
             # Display AI response
