02944737d4fe4b4674df6f679236813cb7e858a9
Ethan <theethancastro@gmail.com>
2023-09-11 20:59:07 -0400

    Update health.py

--- health.py
+++ health.py
@@ -54,4 +54,13 @@ prompt = prompt_template.format(**user_data)
 print(response)  # Debug print statement
 
 
+if response.choices:
+        print(response.choices[0])  # Debug print statement
+        print("Your BMR is:", bmr)
+
+if 'text' in response.choices[0]:
+            # Display AI response
+            st.write(response.choices[0].text.strip())
+
+
   
