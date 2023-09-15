082434a21ffd18cb673d2fb298625a68cbd994ed
Ethan <theethancastro@gmail.com>
2023-09-15 07:16:20 -0400

    Update health.py

--- health.py
+++ health.py
@@ -15,7 +15,10 @@ age = st.number_input('What is your age?', min_value=1, max_value=100)
 weight = st.number_input('What is your weight (in lbs)?', min_value=1)
 height = st.number_input('What is your height (inches)?', min_value=1)
 goal = st.text_input('What is your goal (can be anything health related)?')
-activity = st.number_input('Rate your activity level from sedentary to very active (from 1.2 - 1.9)?', min_value= 1.2, max_value = 1.9)
+activity = st.number_input('Rate your activity level from sedentary (1) to very active (10)?', min_value=1, max_value=10)
+
+# Convert the 1-10 activity scale to 1.2-1.9 scale
+activity_transformed = 1.2 + (activity - 1) * (0.7 / 9)
 bmi = round(weight / (height * height) * 703)
 bmr = round(10 * weight + 6.25 * height - 5 * age - sex)
 tdee = round(bmr * activity)
