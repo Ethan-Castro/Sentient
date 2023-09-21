289af6d6486d8573b1ff33d34d8f3935fb55f5f1
Ethan <theethancastro@gmail.com>
2023-09-21 11:50:03 -0400

    Update and rename health.py to study.py

---
+++ study.py
@@ -0,0 +1,66 @@
+import openai
+import streamlit as st
+
+openai.api_key = st.secrets['openai_key']
+
+st.title('Personal Study Coach by Ethan Castro')
+
+# Get user input
+goal = st.text_input('What topic or subject do you want to study (literally anything!)?')
+time = st.number_input('How long do you want your study plan to be? (days)', min_value=1, max_value=365)
+rigor = st.number_input('How rigorous do you want this to be easy (1) to very rigorous (10)?', min_value=1, max_value=10)
+knowledge = st.number_input('How much do you already know nothing (1) to practically an expert (10)?', min_value=1, max_value=10)
+goal = st.text_input('What is your goal (can be career, mastery, money or related to something else)?')
+name = st.text_input('What is your name so I know who I am talking to?')
+
+# Convert the 1-10 activity scale to 1.2-1.9 scale
+activity = 1.2 + (activity - 1) * .0778
+bmi = round(weight / (height * height) * 703)
+bmr = round(10 * weight + 6.25 * height - 5 * age - sex)
+tdee = round(bmr * activity)
+
+
+
+if st.button('Get advice'):
+    # Generate AI response
+
+    prompt_template = (
+    "This is the script you repeat first 'Hey {name}! /n I am your personal AI study coach, this is not professional advice, but it is helpful ;). Ok so, you want to learn about {goal} and you want to spend around {time} days learning it. Here's the study plan I have for you, Repeat everything before and input the proper values' Then provide advice for the goal, an easy-to follow detailed study plan, a philosophical motivational quote for fulfilling cognitive potential, a piece of advice on mind & body connection. Then end off with an encouraging message. "
+     )
+    
+    user_data = {
+        'name':name,
+        'goal': goal,  # Example age
+        'time': time,  # Example weight in lbs
+        'rigor': height,  # Example height in inches
+        'knowledge': knowledge,  # Example goal
+        'bmi': bmi,  # Example BMI value
+        'tdee': tdee,  # Example TDEE value
+        'advice': goal
+    }
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt = prompt_template.format(**user_data),
+      temperature=.5,
+      max_tokens=500,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+    )
+    
+# You can then format this prompt with the specific details:
+
+    prompt = prompt_template.format(**user_data)
+    print(response)  # Debug print statement
+    
+    
+    if response.choices:
+            print(response.choices[0])  # Debug print statement
+            print("Your BMR is:", bmr)
+    
+    if 'text' in response.choices[0]:
+                # Display AI response
+                st.write(response.choices[0].text.strip())
+    
+
+  
