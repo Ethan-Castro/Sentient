0d64194a406a06cf193ba8dce9f0b5762f9f7460
Ethan <theethancastro@gmail.com>
2023-09-26 17:55:04 -0400

    Update study.py

--- study.py
+++ study.py
@@ -3,57 +3,67 @@ import streamlit as st
 
 openai.api_key = st.secrets['openai_key']
 
-# Streamlit app
+
+# Initialize or update Streamlit session state for maintaining conversation history
+if 'messages' not in st.session_state:
+    st.session_state.messages = [
+        {
+            "role": "system",
+            "content": ("You are an academic coach chatbot designed to support and guide students in their academic "
+                        "journey. With a vast reservoir of knowledge at your disposal, you are well-versed in the "
+                        "resources and strategies from the Harvard Academic Resource Center and other esteemed "
+                        "institutions. Your primary goal is to help students excel academically by providing them "
+                        "with tailored advice, resources, and study strategies. Above all, you approach every "
+                        "interaction with kindness, understanding, and a genuine desire to see students succeed. "
+                        "Whether they're struggling with time management, seeking resources for a specific subject, "
+                        "or just needing a motivational boost, you're here to assist with compassion and expertise.")
+        },
+        {
+            "role": "assistant",
+            "content": "How can I help you today?"
+        }
+    ]
+
 def main():
     st.title("Academic Coach Chatbot")
     st.write("This chatbot is here to help you excel academically using resources from top institutions and professionals.")
-
+    
     user_input = st.text_input("Hey! Introduce yourself, and let me know what you need help on.")
     if user_input:
-        response = get_openai_response(user_input)
-        st.write(f"Bot: {response}")
-
-def get_openai_response(user_message):
-    """Get a response from OpenAI's GPT-4 model using the ChatCompletion method."""
-    response = openai.ChatCompletion.create(
-        model="gpt-4", 
-        messages=[
-     {
-      "role": "system",
-      "content": "    You are an academic coach chatbot designed to support and guide students in their academic journey. With a vast reservoir of knowledge at your disposal, you are well-versed in the resources and strategies from the Harvard Academic Resource Center and other esteemed institutions. Your primary goal is to help students excel academically by providing them with tailored advice, resources, and study strategies. Above all, you approach every interaction with kindness, understanding, and a genuine desire to see students succeed. Whether they're struggling with time management, seeking resources for a specific subject, or just needing a motivational boost, you're here to assist with compassion and expertise\n"
-    },
-    {
-      "role": "assistant",
-      "content": "How can I help you today?"
-    },
-    {
-      "role": "user",
-      "content": user_message,
-    }
-   
-
-  ],
-  temperature=1,
-  max_tokens=1000,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
-    return response.choices[0].message['content']
- if user_input:
         # Append user's message to the conversation history
         st.session_state.messages.append({
             "role": "user",
             "content": user_input
         })
-
-        # Get bot's response and append to the conversation history
+        
         response_content = get_openai_response(user_input)
-        st.messages.append({
+        
+        # Append bot's response to the conversation history
+        st.session_state.messages.append({
             "role": "assistant",
-            "content": response
+            "content": response_content
         })
+        
+        st.write(f"Bot: {response_content}")
 
+def get_openai_response(user_message):
+    """Get a response from OpenAI's GPT-4 model using the ChatCompletion method."""
+    response = openai.ChatCompletion.create(
+        model="gpt-4", 
+        messages=st.session_state.messages,  # Use the maintained conversation history for context
+        temperature=1,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return response.choices[0].message['content']
 
 if __name__ == "__main__":
     main()
+This approach ensures that you keep a history of the conversation, which will allow for better context in subsequent interactions.
+
+
+
+
+
