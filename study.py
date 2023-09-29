1793f239b325c6140741c39878a48a98ebf79446
Ethan <theethancastro@gmail.com>
2023-09-29 15:00:22 -0400

    Update study.py

--- study.py
+++ study.py
@@ -11,20 +11,15 @@ if 'messages' not in st.session_state:
         "role": "system",
         "content": "You are an academic coach chatbot designed to support and guide students in their academic journey. With a vast reservoir of knowledge at your disposal, you are well-versed in the resources and strategies from the Harvard Academic Resource Center and other esteemed institutions. Your primary goal is to help students excel academically by providing them with tailored advice, resources, and study strategies. Above all, you approach every interaction with kindness, understanding, and a genuine desire to see students succeed. Whether they're struggling with time management, seeking resources for a specific subject, or just needing a motivational boost, you're here to assist with compassion and expertise."
     },
-        {
+    {
         "role": "user",
         "content": "I need help with precalculus."
     },
     {
-        "role": "assistant",
-        "content": "I can most def help with that! Whether you’re struggling with limits, derivatives, integrals, or any other calculus topics, feel free to ask, and I’ll do my best to assist you. If you have a specific question or topic in mind, please provide more details, and I can offer more tailored support or resources.
+    "role": "assistant",
+    "content": "I can most def help with that! Whether you’re struggling with limits, derivatives, integrals, or any other calculus topics, feel free to ask, and I’ll do my best to assist you. If you have a specific question or topic in mind, please provide more details, and I can offer more tailored support or resources.\nIf you're looking for general advice or resources, here are a few suggestions:\nUnderstand the Basics: Make sure you have a solid understanding of the fundamental concepts of calculus such as limits, continuity, and derivatives before moving on to more advanced topics.\nPractice Problems: Regularly solving problems is crucial in calculus. Textbooks, online resources, and calculus apps often have a variety of problems to work through.\nOnline Resources: Websites like Khan Academy and Coursera offer free courses on calculus, which can help supplement your learning.\nStudy Groups: Collaborating with peers can provide different perspectives and can be helpful for understanding challenging concepts.\nOffice Hours: Utilizing professor or TA office hours can be beneficial for clarifying doubts and receiving additional support.\nLet me know how you would prefer to proceed, and I'll do my best to assist you!"
+}
 
-If you're looking for general advice or resources, here are a few suggestions: /n Understand the Basics: Make sure you have a solid understanding of the fundamental concepts of calculus such as limits, continuity, and derivatives before moving on to more advanced topics.
-/n Practice Problems: Regularly solving problems is crucial in calculus. Textbooks, online resources, and calculus apps often have a variety of problems to work through. /n Online Resources: Websites like Khan Academy and Coursera offer free courses on calculus, which can help supplement your learning. /n Study Groups: Collaborating with peers can provide different perspectives and can be helpful for understanding challenging concepts.
-
-Office Hours: Utilizing professor or TA office hours can be beneficial for clarifying doubts and receiving additional support.
-Let me know how you would prefer to proceed, and I'll do my best to assist you! "
-    }
 ]
 
 def main():
